974f610ac8193295d0d8d557981facc5af99e6dc
karencatearabit <karen@klab.com>
2013-04-11 13:37:00 +0800

    projects for chapter 2 and 3

---
+++ ch02/AGoodStart/AGoodStart/main.c
@@ -0,0 +1,22 @@
+//
+//  main.c
+//  AGoodStart
+//
+//  Created by Karen Cate Arabit on 4/11/13.
+//  Copyright (c) 2013 Karen Cate Arabit. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    //Print the beginiing of the novel
+    printf("It was the best of times.\n");
+    printf("It was the worst of times.\n");
+    /* Is that actually any good? 
+        Maybe it needs a rewrite. */
+    
+    return 0;
+}
+
