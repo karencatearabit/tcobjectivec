38b420db5ad7ed7575b9ee6b17f165198668caff
karencatearabit <karen@klab.com>
2013-04-12 14:45:44 +0800

    coolness project chapter 7

---
+++ ch07/Coolness/Coolness/main.c
@@ -0,0 +1,29 @@
+//
+//  main.c
+//  Coolness
+//
+//  Created by Karen Cate Arabit on 4/12/13.
+//  Copyright (c) 2013 Karen Cate Arabit. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    //while loop
+//    int i = 0;
+//    while (i < 11) {
+//       printf("%d) KC is awesome\n",i);
+//        i++;
+//    }
+    
+    //for loop
+    for (int i = 0; i < 11; i++) {
+        printf("%d) KC is awesome\n",i);
+    }
+
+
+    return 0;
+}
+
