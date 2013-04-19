efb6986c7b25d74803e0add9914f1a4faf5c63ef
karencatearabit <karen@klab.com>
2013-04-19 17:07:22 +0800

    [C]TwoFloats project Chapter 3

---
+++ ch03/[C] TwoFloats/[C] TwoFloats/main.c
@@ -0,0 +1,22 @@
+//
+//  main.c
+//  [C] TwoFloats
+//
+//  Created by Karen Cate Arabit on 4/19/13.
+//  Copyright (c) 2013 Karen Cate Arabit. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    float a = 3.14;
+    float b = 42.0;
+    double sum;
+    
+    sum = a + b;
+    printf("sum of %f and %f is: %f" ,a ,b ,sum);
+    return 0;
+}
+
