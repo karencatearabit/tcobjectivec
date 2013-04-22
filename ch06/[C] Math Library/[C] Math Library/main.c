cee06a139d1189a035ff92be5a358983aef02b16
karencatearabit <karen@klab.com>
2013-04-22 11:31:26 +0800

    [C] Math Library project Chapter 6

---
+++ ch06/[C] Math Library/[C] Math Library/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  [C] Math Library
+//
+//  Created by Karen Cate Arabit on 4/22/13.
+//  Copyright (c) 2013 Karen Cate Arabit. All rights reserved.
+//
+
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, const char * argv[])
+{
+
+    printf("The sine of 1 radian is %.3f",sinf(1));
+    
+     return 0;
+}
+
